ba7175d0990070f56bdf7f7f4516aad40c6ccb79
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-02-16 22:21:27 -0600

    may accidentally delete some files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,16 @@
 #login
-if status --is-login
+#if status --is-login
     # Environment Variables
-    set -x NPM_PACKAGES "$HOME/.local/npm_packages"
+#    set -x NPM_PACKAGES "$HOME/.local/npm_packages"
     #set -xU NODE_PATH $NPM_PACKAGES/lib/node_modules${NODE_PATH}
-    set -x PATH $HOME/.local/bin $HOME/.cargo/bin $NPM_PACKAGES/bin $PATH
-    set -x MANPATH $NPM_PACKAGES/share/man $MANPATH
-    set -x EDITOR kak
-end
+#    set -x PATH $HOME/.local/bin $HOME/.cargo/bin $NPM_PACKAGES/bin $PATH
+#    set -x MANPATH $NPM_PACKAGES/share/man $MANPATH
+#    set -x EDITOR kak
+#end
 
 # Aliases
 
-alias dfconfig "git --git-dir=$HOME/.config/cfg/ --work-tree=$HOME"
+alias dfconfig "git --git-dir=$HOME/.config/git/cfg/ --work-tree=$HOME"
 
 alias ls "ls -1lih --color=auto"
 
@@ -19,5 +19,5 @@ alias grep="grep --color=auto"
 # Prompt and aesthetics
 
 function fish_prompt
-    powerline-go -error $status -shell bare
+    powerline-rs  $status --shell bare
 end
--- .config/i3/workspaces/coding_workspace.zsh
+++
@@ -1,6 +0,0 @@
-i3-msg workspace\ 2\;\ append_layout\ /home/daedalus/.config/i3/workspaces/coding_workspace.json
-
-(code-oss &)
-(zeal &)
-(termite &)
-(termite &)
